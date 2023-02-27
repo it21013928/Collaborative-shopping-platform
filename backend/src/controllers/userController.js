854286bfcf0ea2c9061937d2f499c3625372216e
it21013928 <it21013928@my.sliit.lk>
2023-02-27 20:31:44 +0530

    implemented user model

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,137 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+
+const User = require("../models/userModel");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // Check name or email or password is empty
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields must be filled" });
+    }
+
+    // Check if email is valid
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ error: "Email is not valid" });
+    }
+
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email already exists" });
+    }
+
+    // Check if password is strong enough
+    if (!validator.isStrongPassword(password)) {
+      return res.status(400).json({
+        error:
+          "Password not strong enough. Must contains uppercase, lowercase, numbers and more than eight characters",
+      });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const user = new User({ name, email, password: hashedPassword, role });
+    await user.save();
+
+    // Create JWT token
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(201).json({ token, userId: user._id, role: user.role });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if email exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Create JWT token
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ token, userId: user._id, role: user.role });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Find user by ID
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update user
+    user.name = name || user.name;
+    user.email = email || user.email;
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+    }
+
+    await user.save();
+
+    res.json({ message: "User updated successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    // Find user by ID
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Delete user
+    await user.remove();
+
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = {
+  deleteUser,
+  updateUser,
+  loginUser,
+  registerUser,
+};
