30f0494d7572496d035e290824b2dca97bd414d1
it21013928 <it21013928@my.sliit.lk>
2023-02-27 18:42:39 +0530

    edited app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,37 @@
+require("dotenv").config();
+
+const port = process.env.PORT;
+
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// express app
 const app = express();
-const port = 3000;
+
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+app.use(cors());
 
 // Set up routes
-const routes = require("./src/routes/index");
-app.use("/", routes);
+const userRoutes = require("./src/routes/userRoutes");
+app.use("/api/users", userRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+
+mongoose.set("strictQuery", false);
+// Connect to the database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for request
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
