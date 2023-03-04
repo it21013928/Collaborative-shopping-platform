69bc8f5d173c839d22e58f3bdca6d4284727c7b8
Kithmina Siriwardana <it21020230@my.sliit.lk>
2023-03-04 11:59:37 +0530

    Product Model, Controllers n Routes are added.

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,27 +1,116 @@
-
 const Product = require("..models/productModel");
 
-const addProduct = async(req, res) => {
-    try {
-        const {name, price, quantity, description } = req.body;
-    
-        //Check if the product name, price, quantiry, description is empty
-        if(!name || !quantity || !price || !description) {
-            return res.status(400).json({ message: "name, quantity, price or description fields are empty" });
-        }
+//CREATE a Product
+const createProduct = async (req, res) => {
+  try {
+    const { name, price, quantity, description } = req.body;
 
-        // Check if product name already exists
-        const existingProduct = await Product.findOne({ name });
-        if (existingProduct) {
-        return res.status(409).json({ message: "Product name already exists" });
-        }
-
-        
-    } catch (error) {
-        
+    //Check if the product name, price, quantiry, description is empty
+    if (!name || !quantity || !price || !description) {
+      return res.status(400).json({
+        message: "name, quantity, price or description fields are empty",
+      });
     }
+
+    // Check if product name already exists
+    const existingProduct = await Product.findOne({ name });
+    if (existingProduct) {
+      return res.status(409).json({ message: "Product name already exists" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//GET Product by ID
+const getProduct = async (req, res) => {
+  try {
+    // Find product
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    //Get product
+    res.send(product);
+    res.json({ message: "Product got successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//GET all products
+const getAllproducts = async (req, res) => {
+  try {
+    // Find product
+    const product = await Product.find();
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.send(product);
+
+    res.json({ message: "All products got successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//Update a product
+const updateProduct = async (req, res) => {
+  try {
+    const { name, price, quantity, decription, image, rating, category } =
+      req.body;
+
+    // Find product by ID
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // Update product
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+    product.decription = decription;
+    product.image = image;
+    product.rating = rating;
+    product.category = category;
+
+    await product.save();
+    res.json({ message: "Product updated successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//Delete a product
+const deleteProduct = async (req, res) => {
+  try {
+    // Find product by ID
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // Delete product
+    await product.remove();
+
+    res.json({ message: "Product deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 module.exports = {
-    addProduct,
-}
+  createProduct,
+  getProduct,
+  getAllproducts,
+  updateProduct,
+  deleteProduct,
+};
--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -1,31 +1,35 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    name: {
-        type: string,
-        required: true,
-    },
-    price: {
-        type: float,
-        required: true,
-    },
-    quantity: {
-        type: number,
-        required: true,
-    },
-    decription: {
-        type: string,
-        required: true,
-    },
-    rating: {
-        type: number,
-        required: false,
-    },
-    category: {
-        type: String,
-        required: false,
-    },
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: float,
+    required: true,
+  },
+  quantity: {
+    type: number,
+    required: true,
+  },
+  decription: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  rating: {
+    type: number,
+    required: false,
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+});
 
 module.exports = mongoose.model("Product", productSchema);
