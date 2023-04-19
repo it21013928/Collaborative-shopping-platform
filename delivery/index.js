4d4216aa0a65aa8481b6205683663c41f2d73dcf
shehan <it21011870@my.sliit.lk>
2023-04-19 08:00:46 +0530

    file uploading 70%

--- delivery/index.js
+++ delivery/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const multer = require("multer");
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT;
@@ -7,8 +6,36 @@ const port = process.env.PORT;
 const mongoose = require("mongoose");
 
 const app = express();
-
+const multer = require("multer");
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 app.use(cors());
+
+/***************************For image uploading ***************************************/
+
+// const fileStorageEngine = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(
+//       null,
+//       "../frontend/src/admin/components/delivery/shippingDetails/uploadedImages"
+//     );
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: fileStorageEngine });
+
+// app.post("/trackingBill", upload.single("image"), (req, res) => {
+//   console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+//   res.send("Single file upload success");
+// });
+/***************************For image uploading ***************************************/
 
 // Connect to the database
 mongoose
@@ -16,7 +43,7 @@ mongoose
   .then(() => {
     // listen for request
     app.listen(port, () => {
-      console.log(`Order is listening at http://localhost:${port}`);
+      console.log(`Delivery is listening at http://localhost:${port}`);
     });
   })
   .catch((error) => {
@@ -24,24 +51,5 @@ mongoose
   });
 
 //Routes
-const orderRoutes = require("./src/routes/deliveryRoutes");
-app.use("/", orderRoutes);
-
-/***************************For image uploading ***************************************/
-
-const fileStorageEngine = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "../frontend/src/admin/components/delivery/shippingDetails");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: fileStorageEngine });
-
-app.post("/trackingBill", upload.single("image"), (req, res) => {
-  console.log(req.file);
-  res.send("Single file upload success");
-});
-/***************************For image uploading ***************************************/
+const deliveryRoutes = require("./src/routes/deliveryRoutes");
+app.use("/", deliveryRoutes);
